db48fcf5503324b2b3d29819397e19320d102556
DevJonatha <jonathalarasilverio@gmail.com>
2024-02-01 14:25:12 -0300

    jsx e js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import Hashtaurante from "./assets/hashtaurante.webp";
 import './App.css';
 import { Navegacao } from "./Navegacao";
 import { ItemCardapio } from "./ItemCardapio";
+import {pratosPrincipais, sobremesas, bebidas} from "./cardapio.js";
 
 export function App() {
 
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atuzalizarPaginaSelecionada] = useState(0);
   return <>
     <img src={Hashtaurante} alt="Hashtaurante" className="capa"/>
-    <Navegacao></Navegacao>
-    <ItemCardapio></ItemCardapio>
-  
-  </>
+    <Navegacao atuzalizarPaginaSelecionada={atuzalizarPaginaSelecionada}></Navegacao>
+      <div className="menu">
+        { paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem} /> )};
+        { paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem} /> )};
+        { paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem} /> )};
+      </div>
+    </>
 }
